dd3314c357b2bb8aeec2b003bc4a1ec7370c77bb
Isaac Orme <isorme1@gmail.com>
2017-12-03 18:26:09 -0800

    Only admins can create or delete projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "application#index"
+
+    resources :projects, only: [:new, :create, :destroy]
   end
 
   devise_for :users
-  resources :projects do
+  resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
 
--- spec/features/admin/deleting_projects_spec.rb
+++ spec/features/admin/deleting_projects_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature "Users can delete projects" do
+  before do
+    login_as(FactoryGirl.create(:user, :admin))
+  end
+  
   scenario "successfully" do
     FactoryGirl.create(:project, name: "Sublime Text 3")
 
@@ -12,4 +16,4 @@ RSpec.feature "Users can delete projects" do
     expect(page.current_url).to eq projects_url
     expect(page).to have_no_content "Sublime Text 3"
   end
-end 
+end
